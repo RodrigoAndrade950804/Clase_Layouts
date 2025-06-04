c3c7db2c2dafe4ec674c3145bfb9f785e3423362
Rodrigo Andrade <rodrigo.andrade@hotmail.com>
2025-06-03 20:30:04 -0500

    Agregar archivos de proyecto.

---
+++ Clase_Layouts/Clase_Layouts/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Clase_Layouts
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Clase_Layouts/Clase_Layouts/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Clase_Layouts
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
